291c2d91443c8029efd44ecbba4693343cfb0b7b
SANTIAGO COLMENARES BOLIVAR <70450482+stg077@users.noreply.github.com>
2026-04-02 10:08:33 -0500

    Clean up commented shutdown code in main.py
    Removed commented code for app shutdown resources.

--- app/main.py
+++ app/main.py
@@ -8,8 +8,6 @@ async def lifespan(app: FastAPI):
     # --- Código que corre al INICIAR la app (Docker / Producción) ---
     init_db()
     yield
-    # --- Código que corre al CERRAR la app (si lo necesitas) ---
-    # print("Cerrando recursos...")
 
 app=FastAPI(title="Prueba LOGYCA Santiago Colmenares",
     description="Backend para automatización de lectura de CVS Azure y PostgreSQL",
